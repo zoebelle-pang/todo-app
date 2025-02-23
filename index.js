0745d5efddbd144ca9a0e4215181c720f5672ca4
Interndev-zoebelle <interndev@ohceans.com>
2025-02-24 02:01:09 +0800

    Code refactoring on mongodb

--- index.js
+++ index.js
@@ -1,35 +1,40 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const path = require("path")
+const bodyParser = require("body-parser");
+const moment = require("moment");
+const connectMongodb = require("./init/mongodb")
 
 const PORT = 8000;
 
 // Init app
 const app = express();
 
-const connectionUrl = "mongodb://localhost:27017/todoDb";
-
-
-
-mongoose
-.connect(connectionUrl)
-.then(() => console.log("Database connection successful"))
-.catch((error) => console.log(error.message));
+//mongodb connection
+connectMongodb();
 
 const todoSchema = mongoose.Schema({
     title: {type: String, required: true }, 
     desc: String,
-}, {timestamps: true}
+}, 
+{timestamps: true}
 );
+
+const Todo = mongoose.model("todo", todoSchema);
 // View engine
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname,"public")));
+app.use(bodyParser.urlencoded({extended: true}));
 
-app.get("/", (req, res, next) => {
+
+app.get("/", async(req, res, next) => {
     try{
-      res.render("index");
+      const todos = await Todo.find({}).sort({ createdAt: -1 });
+
+      res.locals.moment = moment
+      res.render("index", { title: "List todo", todos: todos });
      }catch(error){
-        res.status(500).jason({message: error.message});
+        res.status(500).json({message: error.message});
      }
 });
 
@@ -56,6 +61,24 @@ app.get("/delete-todo", (req, res, next) => {
         res.status(500).json({ message: error.message });
     }
 });
+
+app.post("/add-todo", async (req, res, next) => {
+    try{
+        const {title, desc} = req.body;
+
+        if(!title){
+            return res.status(400).json({message: "Title is required" });
+        }
+
+        const newTodo = new Todo({title, desc});
+        await newTodo.save();
+
+        res.redirect("/")
+    } catch (error) {
+    res.status(500).json({ message: error.message })
+    }
+})
+
 
 // Listen to the server
 app.listen(PORT, () => {
